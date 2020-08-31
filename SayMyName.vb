124945cc96873f8053ade5d36080b6366c91e789
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-08-30 19:54:26 -0600

    add question and reply

--- SayMyName.vb
+++ SayMyName.vb
@@ -10,7 +10,15 @@ Option Explicit On
 Module SayMyName
 
     Sub Main()
+        Dim userMessage As String
 
+        'ask user "What's your name?"
+        Console.WriteLine("What's your name?")
+        'collect user's message
+        userMessage = Console.ReadLine()
+        'dispay the message "Hello, what user entered when your done press enter."
+        Console.WriteLine("Hello, " & userMessage & " when you're done press enter.")
+        Console.ReadLine()
     End Sub
 
 End Module
