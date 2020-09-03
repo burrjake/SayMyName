70836905a3526c2a54cf4bad0ba69387331d4721
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-09-03 08:30:38 -0600

    add if for if Jake, Jake Burrell, Emily, Joe,, or someone else enters their name

--- SayMyName.vb
+++ SayMyName.vb
@@ -6,6 +6,7 @@
 
 Option Strict On
 Option Explicit On
+Option Compare Text
 
 Module SayMyName
 
@@ -16,9 +17,18 @@ Module SayMyName
         Console.WriteLine("What's your name?")
         'collect user's message
         userMessage = Console.ReadLine()
-        'dispay the message "Hello, what user entered when your done press enter."
-        Console.WriteLine("Hello, " & userMessage & " when you're done press enter.")
+        If userMessage = "Jake" Or userMessage = "Jake Burrell" Then
+            'display the mssage "Hello, creator." if Jake or Jake Burrell is entered for the name
+            Console.WriteLine("Hello, creator.")
+        ElseIf userMessage = "Emily" Or userMessage = "Joe" Then
+            'dispay the message "Hello, Joe or Emily. When your done press enter."
+            Console.WriteLine("Hello, " & userMessage & ". When you're done press enter.")
+        Else
+            'display the message "Hello there. When your done press enter." if the user is not Jake, Jake Burrell, Emily, or Joe
+            Console.WriteLine("Hello there. When your done press enter.")
+        End If
         Console.ReadLine()
+
     End Sub
 
 End Module
